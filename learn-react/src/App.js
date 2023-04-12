32c36927029deb8db23679beec522083aca641e6
NAMAN TYAGI <namantyagi33@gmail.com>
2023-04-12 19:16:53 +0530

    react lec-1

---
+++ learn-react/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+
+<div>
+<div>
+<p> this is a react component</p>
+
+</div>
+<div>
+
+<p> this is another nested element</p>
+
+</div>
+</div>
+
+  );
+}
+
+export default App;
---
+++ learn-react/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import Person from './components/Person';
+import TodoList from './components/TodoList';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ 
+   <div>
+
+    <TodoList />
+
+    {/* <App />
+    <App />
+     */}
+   </div>
+);
+
